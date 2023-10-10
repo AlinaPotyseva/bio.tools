bb3e26378d4ad2aaf0ad43afe16d011fcb77f1d3
Alina <alina.potyseva@gmail.com>
2023-10-11 00:11:20 +0300

    Add gc_count function in fastaq_tool

---
+++ modules/fastaq_tool.py
@@ -0,0 +1,19 @@
+def gc_count(seq: tuple, gc_bounds: int or float or tuple = (0, 100)) -> bool:
+    """
+    Function gc_count counts GC-content of the inputed sequence. It returns
+    True if GC-content is in a given interval (gc_bounds), else it returns
+    False.
+    :param seq: consists of two strings: sequence and quality, type: tuple
+    :param gc_bounds: the interval for filtering, type: tuple if both edges of
+    the interval is given or int or float if only right edge is given, default
+    is (0, 100)
+    :return: True if gc-content of the sequence is in gc_bounds, else False
+    """
+    if type(gc_bounds) == int or type(gc_bounds) == float:
+        gc_bounds = (0, gc_bounds)
+    gc = 0
+    for nt in seq[0]:
+        if nt == 'G' or nt == 'C':
+            gc += 1
+    result = 100 * gc / len(seq[0])
+    return gc_bounds[0] <= result <= gc_bounds[1]
