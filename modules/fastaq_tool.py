08f3a7d89cf97e59ac6559fcf0c51f623cd00306
Alina <alina.potyseva@gmail.com>
2023-10-11 00:14:15 +0300

    Add length_count function in fastaq_tool

--- modules/fastaq_tool.py
+++ modules/fastaq_tool.py
@@ -17,3 +17,19 @@ def gc_count(seq: tuple, gc_bounds: int or float or tuple = (0, 100)) -> bool:
             gc += 1
     result = 100 * gc / len(seq[0])
     return gc_bounds[0] <= result <= gc_bounds[1]
+
+def length_count(seq: tuple, length_bounds: int or tuple = (0, 2 ** 32)) \
+        -> bool:
+    """
+    Function counts the length of the sequence. It returns True if the length
+    of the sequence is in the given length_bounds, else it returns False.
+    :param seq: consists of two strings: sequence and quality, type: tuple
+    :param length_bounds: the interval for filtering, type: tuple if both
+    edges of the interval is given or int if only right edge is given, default
+    is (0, 2 ** 34)
+    :return: True if the length of the sequence is in length_bounds, else False
+    """
+    if type(length_bounds) == int:
+        length_bounds = (0, length_bounds)
+    result = len(seq[0])
+    return length_bounds[0] <= result <= length_bounds[1]
