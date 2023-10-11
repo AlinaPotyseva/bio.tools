cc06836f7dca913f965a9419ef95c4e3022a7dd4
Alina <alina.potyseva@gmail.com>
2023-10-11 03:28:32 +0300

    Add dna_rna_tool_running function in bio_tools

--- bio_tools.py
+++ bio_tools.py
@@ -1,4 +1,5 @@
 import modules.fastaq_tool as fqt
+import modules.dna_rna_tool as drt
 
 def fastaq_tool_running(seqs: dict, gc_bounds: int or float or tuple = (0, 100),
           length_bounds: int or tuple = (0, 2 ** 32),
@@ -27,3 +28,34 @@ def fastaq_tool_running(seqs: dict, gc_bounds: int or float or tuple = (0, 100),
                 fqt.quality_check(seqs[seq][1], quality_threshold):
             result[seq] = seqs[seq]
     return result
+    
+def dna_rna_tool_running(*input_data: str) -> str or list:
+    """
+    Function takes as input an arbitrary number of arguments with DNA or RNA
+    sequences (*str*) and the name of the procedure to be executed (it is
+    always the last argument, *str*, see example of use). After that, the c
+    ommand performs the specified action on all submitted sequences.
+    If one sequence is submitted, a string with the result is returned.
+    If more than one sequence is submitted, a list of strings is returned.
+    :param input_data: str of nucleic acids and a procedure
+    :return: string or list of data
+    """
+    results = []
+    nucleic_acids = list(input_data)[:-1]
+    for nucleic_acid in nucleic_acids:
+        if drt.check_input_for_nucleic_acids(nucleic_acid):
+            if list(input_data)[-1] == 'transcribe':
+                results.append(drt.transcribe(list(nucleic_acid)))
+            if list(input_data)[-1] == 'reverse':
+                results.append(drt.reverse(list(nucleic_acid)))
+            if list(input_data)[-1] == 'complement':
+                results.append(drt.complement(list(nucleic_acid)))
+            if list(input_data)[-1] == 'reverse_complement':
+                results.append(drt.reverse_complement(list(nucleic_acid)))
+        else:
+            pass
+    if len(results) == 1:
+        results_str = str(results[0])
+        return results_str
+    else:
+        return results
