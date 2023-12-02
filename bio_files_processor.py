1a84f79ad4f82834b0bc6f7e7d1b3bb8c83b3ad2
Potyseva Alina <alina.potyseva@gmail.com>
2023-12-02 13:14:22 +0300

    Add change_fasta_start_pos

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -29,3 +29,42 @@ def convert_multiline_fasta_to_oneline(input_fasta, output_fasta=''):
                     count += 1
                 output_fasta.write(string+'\n')
                 line_number = count
+
+def change_fasta_start_pos(input_fasta: str, shift: int, output_fasta: str = None):
+    """
+    Function shifts the starting position in the file
+    :param input_fasta: the path to the fasta file
+    :param shift: integer (can be negative) - by how much to shift the initial 
+    position in the file
+    :param output_fasta: the name of output file, if not given adds 'out' at
+    the beginning of the input file
+    :return:  a fasta file with shifted sequence
+    """
+
+    if os.path.isfile(input_fasta) == False:
+        print('The path does not exist!')
+    with open(input_fasta, 'r') as fasta_file:
+        lines = fasta_file.readlines()
+    with open(os.path.join(input_fasta)):
+        for line in lines:
+            if not line.startswith('>'):
+                line = line
+                if shift > 0:
+                    first_part_line = line[:shift]
+                    last_part_line = line[shift+1:]
+                    shifting_nucleotide = line[shift]
+                    result_sequence = shifting_nucleotide + first_part_line + last_part_line
+                elif shift == -1:
+                    shifting_nucleotide = line[shift]
+                    first_part_line = line[:shift]
+                    result_sequence = shifting_nucleotide + first_part_line
+    output_dir = os.path.dirname(input_fasta)
+    if output_fasta == None:
+        input_fasta2 = os.path.basename(input_fasta) + '_2'
+        output_fasta_2 = os.path.basename(input_fasta2)
+    elif output_fasta.find('.fasta') == False:
+        output_fasta_2 = output_fasta + '.fasta'
+    else:
+        output_fasta_2 = output_fasta
+    with open(os.path.join(output_dir, output_fasta_2), mode='w') as file:
+        file.write(result_sequence)
